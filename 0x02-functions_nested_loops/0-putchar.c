ff59a74798b0032d5ad1958aae6b510ffb815761
Francis-Kasamba <kasambafrancis4@gmail.com>
2023-10-11 13:36:02 -0700

    readme

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,10 @@
 #include "main.h"
 
+/*
+ * main - Entry point of the program
+ */
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-          return (0);
+    _putchar(_);
+    return (0);
 }
-
