f6c68e220468f78b895404c34e5c741d52b3897d
Francis-Kasamba <kasambafrancis4@gmail.com>
2023-10-16 14:55:06 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,34 +3,36 @@
 /**
  * _atoi - convert a string to an integer
  * @s: the pointer to convert
- * Return: A integer
+ * Return: An integer
  */
 
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+    int c = 0;
+    unsigned int ni = 0;
+    int min = 1;  /* Initialize the sign to positive */
+    int isi = 0;
 
-	while (s[c])
-	{
-		if (s[c])
-		{
-			min *= -1;
-		}
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (isi == 1)
-		{
-			break;
-		}
-		c++;
-	}
-	ni *= min;
-	return (ni);
+    while (s[c])
+    {
+        if (s[c] == '-')
+        {
+            min *= -1;  /* Change the sign to negative if a '-' is encountered */
+        }
+        while (s[c] >= '0' && s[c] <= '9')
+        {
+            isi = 1;
+            ni = (ni * 10) + (s[c] - '0');
+            c++;
+        }
+        if (isi == 1)
+        {
+            break;
+        }
+        c++;
+    }
+    ni *= min;  /* Apply the sign to the final result */
+
+    return ni;  /* Return the converted integer */
 }
+
