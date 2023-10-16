935d7a9aa910a97513f21e0a149bbf37b9f9d247
Francis-Kasamba <kasambafrancis4@gmail.com>
2023-10-16 12:34:54 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Compute the length of a string.
+ * @S: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *S)
+{
+    int i;
+    int count = 0;
+
+    for (i = 0; S[i] != '\0'; i++)
+    {
+        count++;
+    }
+
+    return count;
+}
+
