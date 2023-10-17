8c63e108153de9f65cf0b4fb41254940e1062ecf
Francis-Kasamba <kasambafrancis4@gmail.com>
2023-10-17 08:44:42 -0700

    101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,48 +10,48 @@
  */
 void print_times_table(int n)
 {
-    int num, mult, prod;
-
-    if (n >= 0 && n <= 15)
-    {
-        for (num = 0; num <= n; num++)
-        {
-            _putchar('0');
-            for (mult = 1; mult <= n; mult++)
-            {
-                prod = num * mult;
-
-                if (mult == 0)
-                    prod = 0;
-
-                if (mult == 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar('0' + prod);
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-
-                    if (prod <= 99)
-                        _putchar(' ');
-
-                    if (prod <= 9)
-                        _putchar(' ');
-
-                    if (prod > 99)
-                        _putchar('0' + prod / 100);
-
-                    if (prod > 9)
-                        _putchar('0' + (prod / 10) % 10);
-
-                    _putchar('0' + prod % 10);
-                }
-            }
-            _putchar('\n');
-        }
-    }
+	int num, mult, prod;
+	
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				prod = num * mult;
+				
+				if (mult == 0)
+					prod = 0;
+				
+				if (mult == 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + prod);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					
+					if (prod <= 99)
+						_putchar(' ');
+					
+					if (prod <= 9)
+						_putchar(' ');
+					
+					if (prod > 99)
+						_putchar('0' + prod / 100);
+					
+					if (prod > 9)
+						_putchar('0' + (prod / 10) % 10);
+					
+					_putchar('0' + prod % 10);
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
 
