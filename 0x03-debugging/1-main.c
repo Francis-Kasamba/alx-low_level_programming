3b5525d8314496e3ef994a6c89baba239714437f
Francis-Kasamba <kasambafrancis4@gmail.com>
2023-10-22 06:38:17 -0700

    main-c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Infinite loop incoming :(\n");
+    /*
+     * i = 0;
+     *
+     * while (i < 10)
+     * {
+     *     putchar(i);
+     * }
+     */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
+
