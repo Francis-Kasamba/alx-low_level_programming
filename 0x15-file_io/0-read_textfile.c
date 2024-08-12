8ffcc9e5ca564c9809f092b543ca7a526aa2aa82
Francis-Kasamba <kasambafrancis4@gmail.com>
2024-08-12 14:57:39 -0700

    task1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,41 @@
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
- *
- * Return: The actual number of letters it could read and print, or 0 on failure.
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	ssize_t file, let, w;
+	char *text;
 
-    if (filename == NULL)
-        return (0);
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	file = open(filename, O_RDONLY);
 
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	let = read(file, text, letters);
 
-    free(buffer);
-    close(fd);
-    return (bytes_written);
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
-
